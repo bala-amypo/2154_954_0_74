88b0a8442ac8c01be3c117e26d5467a8919ef432
bala-amypo <bala@amypo.in>
2025-12-17 06:11:51 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/Studserviceimpl.java
+++ src/main/java/com/example/demo/service/Impl/Studserviceimpl.java
@@ -4,5 +4,30 @@ public class Studentity{
     private Long id;
     private String name;
     private Sting email;
-    public Long 
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Studentity(int id,String name,String email) {
+        this.id=(long)id;
+        this.name=name;
+        this.email=email;
+    }
+    public Studentity(){
+        
+    }
 }
